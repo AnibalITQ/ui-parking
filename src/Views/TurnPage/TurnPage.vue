6ef77f80e0100a80dca0a6b9195fff6e621e3910
Tirado Delgado Anibal Emilio <124332136+AnibalITQ@users.noreply.github.com>
2025-02-19 00:15:22 -0600

    Create TurnPage.vue

---
+++ src/Views/TurnPage/TurnPage.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import UserCard from "@/components/PlaceCard.vue";
+
+const handleConfirm = () => {
+  console.log("Usuario confirmado");
+};
+</script>
+
+<template>
+  <UserCard title="Turno asignado" number="3" @confirm="handleConfirm" />
+</template>
