801535be3a1efe5cc1e043ec53f85c3a9fcb4290
Tirado Delgado Anibal Emilio <124332136+AnibalITQ@users.noreply.github.com>
2025-04-30 19:59:51 -0600

    fixes test

--- vite.config.ts
+++ vite.config.ts
@@ -1,64 +1,66 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import { VitePWA } from "vite-plugin-pwa";
-import path from "path";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa';
+import path from 'path';
 
-export default defineConfig(({ mode }) => {
+export default defineConfig(() => {
+  // Usamos la variable de entorno VITE_BASE_URL o establecemos un valor por defecto
+  const base = process.env.VITE_APP_BASE_URL || '/';
+  console.log('Base URL from environment:', process.env.VITE_APP_BASE_URL);
 
-  const base = mode === 'production' ? '/' : '/ui-parking/';
+  console.log(`Base URL is set to: ${base}`);
 
-  return{
-  base,
-  plugins: [
-    vue(),
-    VitePWA({
-      registerType: "autoUpdate",
-      manifest: {
-        name: "Parking ITQ",
-        short_name: "Parking",
-        description: "Gestión inteligente de estacionamientos para el ITQ",
-        lang: "es-ES",
-        start_url: "/",
-        scope: "/",
-        display: "standalone",
-        orientation: "portrait",
-        background_color: "#E5700F",
-        theme_color: "#E5700F",
-        icons: [
-          {
-            src: "/android-chrome-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "/android-chrome-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-        ],
-        // Ejemplo de accesos directos
-        shortcuts: [
-          {
-            name: "Mis QR",
-            short_name: "Mis QR",
-            description: "Accede rápidamente a tus códigos QR",
-            url: "/home",
-            icons: [
-              {
-                src: "/icons/favicon-16x16.png",
-                sizes: "192x192",
-                type: "image/png",
-              },
-            ],
-          },
-        ],
+  return {
+    base,
+    plugins: [
+      vue(),
+      VitePWA({
+        registerType: 'autoUpdate',
+        manifest: {
+          name: 'Parking ITQ',
+          short_name: 'Parking',
+          description: 'Gestión inteligente de estacionamientos para el ITQ',
+          lang: 'es-ES',
+          start_url: '/',
+          scope: '/',
+          display: 'standalone',
+          orientation: 'portrait',
+          background_color: '#E5700F',
+          theme_color: '#E5700F',
+          icons: [
+            {
+              src: '/android-chrome-192x192.png',
+              sizes: '192x192',
+              type: 'image/png',
+            },
+            {
+              src: '/android-chrome-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+            },
+          ],
+          shortcuts: [
+            {
+              name: 'Mis QR',
+              short_name: 'Mis QR',
+              description: 'Accede rápidamente a tus códigos QR',
+              url: '/home',
+              icons: [
+                {
+                  src: '/icons/favicon-16x16.png',
+                  sizes: '192x192',
+                  type: 'image/png',
+                },
+              ],
+            },
+          ],
+        },
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
       },
-    }),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
     },
-  },
-}
+  };
 });
